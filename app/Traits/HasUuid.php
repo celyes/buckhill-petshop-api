1726126d73d882438fba647d97750309b3438b2a
celyes <celyes02@gmail.com>
2024-07-03 16:02:52 +0100

    feat: add UUID support for necessary models

---
+++ app/Traits/HasUuid.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Traits;
+
+use Illuminate\Support\Str;
+
+trait HasUuid
+{
+    protected static function boot()
+    {
+        parent::boot();
+        static::creating(function ($model) {
+            $model->uuid = (string) Str::uuid();
+        });
+    }
+    public function getIncrementing(): bool
+    {
+        return false;
+    }
+    public function getKeyType(): string
+    {
+        return 'string';
+    }
+}
