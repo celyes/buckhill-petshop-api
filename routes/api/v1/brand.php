c40d143fc0654eff74723eb8c465cb32f3c9ccdf
celyes <celyes02@gmail.com>
2024-07-05 16:46:09 +0100

    fix: routing conflict in brands

--- routes/api/v1/brand.php
+++ routes/api/v1/brand.php
@@ -4,3 +4,9 @@ use App\Http\Controllers\BrandController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/{brand}', [BrandController::class, 'show']);
+
+Route::middleware(['jwt', 'verified'])->group(function () {
+    Route::post('/create', [BrandController::class, 'create']);
+    Route::put('/{brand}', [BrandController::class, 'edit']);
+    Route::delete('/{brand}', [BrandController::class, 'delete']);
+});
--- routes/api/v1/brands.php
+++ routes/api/v1/brands.php
@@ -6,9 +6,3 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', [BrandController::class, 'index']);
-
-Route::middleware(['jwt', 'verified'])->group(function () {
-    Route::post('/create', [BrandController::class, 'create']);
-    Route::put('/{brand}', [BrandController::class, 'edit']);
-    Route::delete('/{brand}', [BrandController::class, 'delete']);
-});
